bff27fc3b35c0552f24f426fe7d7449e2a153bfd
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-17 13:41:00 +0900

    feat: テーブル追加

--- app/src/components/VendingModal.tsx
+++ app/src/components/VendingModal.tsx
@@ -1,6 +1,6 @@
 import { Drink } from "@/lib/@type/drink";
 import { VendingType } from "@/lib/@type/vending";
-import { Box, Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { FC } from "react";
 
 interface VendingModalProps {
@@ -13,26 +13,43 @@ interface VendingModalProps {
 export const VendingModal: FC<VendingModalProps> = ({ isOpen, vending, drinks, onClose }) => {
 
     return <>
-        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <Modal onClose={onClose} isOpen={isOpen} isCentered size="6xl">
             <ModalOverlay />
             <ModalContent>
                 <ModalCloseButton />
                 <ModalBody>
-                    <Flex>
+                    <Flex justifyContent='center'>
                         <Box>
                             <Text>{vending.address}</Text>
                             <Text>{vending.pay}</Text>
                             <Text>x:{vending.location_x}, y:{vending.location_y}</Text>
                         </Box>
-                        <Box>
-                            {
-                                drinks.map((drink, index) => {
-                                    return <Box key={index}>
-                                        {JSON.stringify(drink)}
-                                    </Box>
-                                })
-                            }
-                        </Box>
+                        <TableContainer>
+                            <Table variant='simple'>
+                                <Thead>
+                                    <Tr>
+                                        <Th>商品名</Th>
+                                        <Th isNumeric>価格</Th>
+                                        <Th>温度</Th>
+                                        <Th>カテゴリー</Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {
+                                        drinks.map((drink, index) => {
+                                            return <Tr key={index}>
+                                                <Td>{drink.product_name}</Td>
+                                                <Td isNumeric>{drink.price}</Td>
+                                                <Td>{drink.temp}</Td>
+                                                <Td>{drink.category}</Td>
+                                            </Tr>
+                                        })
+                                    }
+                                    <Tr>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+                        </TableContainer>
                     </Flex>
                 </ModalBody>
             </ModalContent>

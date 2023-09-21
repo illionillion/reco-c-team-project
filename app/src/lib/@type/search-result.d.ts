c159a4fa1fda4cc5072053489a26ce89c3a94785
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-22 00:57:53 +0900

    add: 検索結果の型追加

---
+++ app/src/lib/@type/search-result.d.ts
@@ -0,0 +1,16 @@
+import { Drink } from "./drink";
+import { VendingType } from "./vending";
+
+export interface SearchResult {
+  address: VendingType["address"];
+  vid: VendingType["id"];
+  location_x: VendingType["location_x"];
+  location_y: VendingType["location_y"];
+  pay: VendingType["pay"];
+  did: Drink["did"];
+  product_name: Drink["product_name"];
+  price: Drink["price"];
+  temp: Drink["temp"];
+  category: Drink["category"];
+  url: Drink["url"];
+}

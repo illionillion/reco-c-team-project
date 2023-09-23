abc7bd07b966998a993122c8affa497a70915fa5
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-23 10:55:39 +0900

    feat: ドロワーに結果表示
    クリック時にズームをやる

--- app/src/components/ResultDrawer.tsx
+++ app/src/components/ResultDrawer.tsx
@@ -1,5 +1,5 @@
 import type { SearchResult } from '@/lib/@type/search-result';
-import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader } from '@chakra-ui/react';
+import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, Flex, Image, Text } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 interface ResultDrawerProps {
@@ -23,12 +23,22 @@ export const ResultDrawer: FC<ResultDrawerProps> = ({ isOpen, searchWord, search
           <DrawerCloseButton />
           <DrawerHeader>検索飲み物「{searchWord}」</DrawerHeader>
 
-          <DrawerBody>
-            {searchResult.map((item, index) => {
-              return (
-                <Box key={index}></Box>
-              );
-            })}
+          <DrawerBody p={0}>
+            <Flex flexDirection='column'>
+              {searchResult.map((item, index) => {
+                return (
+                  <Flex key={index} py={4} px={6} borderTop='1px solid #000'>
+                      <Box textAlign="center" flex={1}>
+                        <Image m="auto" width="30px" src={item.url} />
+                        <Text>{item.product_name}</Text>
+                      </Box>
+                      <Flex flex={2} justify='center' alignItems='center'>
+                        {parseInt(item.price)}円
+                      </Flex>
+                  </Flex>
+                );
+              })}
+            </Flex>
           </DrawerBody>
 
         </DrawerContent>

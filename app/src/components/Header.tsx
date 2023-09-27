e0e518e68a99ff13623aaa98839100cb8c154f11
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-28 00:17:37 +0900

    fix: 親をformタグに変更

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,30 +1,30 @@
 import { Box, Button, Input, InputGroup, InputLeftElement, InputRightAddon, Text } from '@chakra-ui/react';
-import type { FC, KeyboardEvent, RefObject } from 'react';
+import type { FC, FormEvent, RefObject } from 'react';
 import { BsSearch } from 'react-icons/bs';
 interface HedaerProps {
   searchInputRef: RefObject<HTMLInputElement> | null
   submitSearch: () => Promise<void>
   onResultDrawerOff: () => void
 }
-export const Header: FC<HedaerProps> = ({searchInputRef, submitSearch, onResultDrawerOff }) => {
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.nativeEvent.isComposing || e.key !== 'Enter') return
+export const Header: FC<HedaerProps> = ({ searchInputRef, submitSearch, onResultDrawerOff }) => {
+  const handleSubmitSearch = (e: FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
     submitSearch()
   }
   return (
     <Box as="header" w='100vw' h="16" backgroundColor='blue.700' position='sticky' top={0} px={6} display='flex' justifyContent='center' alignItems='center'>
-      <InputGroup flex={1} borderRadius={5} backgroundColor='white' w="50%">
+      <InputGroup as="form" flex={1} onSubmit={handleSubmitSearch} borderRadius={5} backgroundColor='white' w="50%">
         <InputLeftElement
           pointerEvents="none"
         >
           <BsSearch color="gray.600" />
         </InputLeftElement>
-        <Input ref={searchInputRef} type="search" placeholder="商品検索" onKeyDown={handleKeyDown} onChange={e => {
+        <Input ref={searchInputRef} type="search" placeholder="商品検索" onChange={e => {
           if (e.currentTarget.value === "") {
             // モーダルを閉じる
             onResultDrawerOff()
           }
-        }}  />
+        }} />
         <InputRightAddon
           p={0}
           border="none"
